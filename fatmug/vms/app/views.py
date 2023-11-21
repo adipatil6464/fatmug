4c6ec26e9afdd44236c8ff386989df243d1c2085
adipatil6464 <91333594+adipatil6464@users.noreply.github.com>
2023-11-21 17:22:05 +0530

    purchase order views

--- fatmug/vms/app/views.py
+++ fatmug/vms/app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
 # Create your views here.
-def home(request):
-    return HttpResponse("hey")
+def purchase_orders(request):
+    if request.POST == 'POST':
+        po_number = request.POST['po_number']
+        vendor = request.POST['vendor']
+        order_date = datetime.now()
+        delivery_date = request.POST['delivery_date']
+        items = request.POST['items']
+        quantity = request.POST['quantity']
+        status = request.POST['status']
+        quality_rating = request.POST['quality_rating']
+        issue_date = request.POST['issue_date']
+        acknowledgment_date = request.POST['acknowledgment_date']
 
 
-#C:\fatmug\fatmug\vms
+
