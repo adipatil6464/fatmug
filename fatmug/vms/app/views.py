dd853307e356e5767c9f92a33f444f4b9cee6ca3
adipatil6464 <91333594+adipatil6464@users.noreply.github.com>
2023-11-21 23:55:58 +0530

    PO view and PO form

--- fatmug/vms/app/views.py
+++ fatmug/vms/app/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from datetime import datetime
+from app.models import *
 # Create your views here.
-def purchase_orders(request):
-    if request.POST == 'POST':
-        po_number = request.POST['po_number']
-        vendor = request.POST['vendor']
-        order_date = datetime.now()
-        delivery_date = request.POST['delivery_date']
-        items = request.POST['items']
-        quantity = request.POST['quantity']
-        status = request.POST['status']
-        quality_rating = request.POST['quality_rating']
-        issue_date = request.POST['issue_date']
-        acknowledgment_date = request.POST['acknowledgment_date']
+from django.shortcuts import render
+from .models import Vendor
 
+def purchase_orders(request):
+    VENDOR = Vendor.objects.all()
+    
+    if request.method == 'POST':
+        po_number = request.POST.get('po_number')
+        vendor = request.POST.get('vendor')
+        order_date = request.POST.get('order_date')
+        delivery_date = request.POST.get('delivery_date')
+        items = request.POST.get('items')
+        quantity = request.POST.get('quantity')
+        status = request.POST.get('status')
+        quality_rating = request.POST.get('quality_rating')
+        issue_date = request.POST.get('issue_date')
+        acknowledgment_date = request.POST.get('acknowledgment_date')
+
+        ven = Vendor.objects.get(id=vendor)
+        POModel.objects.create(po_number=po_number, vendor=ven, order_date=order_date, delivery_date=delivery_date, items=items, quantity=quantity, status=status, quality_rating=quality_rating, issue_date=issue_date, acknowledgment_date=acknowledgment_date)
+        # return redirect('home',{'VENDOR': VENDOR})
+    # This line will cause an error as variables are accessed outside the if block
+    # print(po_number, vendor, order_date, delivery_date, items, quantity, status, quality_rating, issue_date, acknowledgment_date)
+
+    return render(request, 'purchaseorder.html', {'VENDOR': VENDOR})
 
 
